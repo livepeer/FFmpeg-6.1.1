f69be21aad4a03d9c1deac8ec4ed35181f5f90d8
Ivan Poleshchuk <ivan.poleshchuk@outlook.com>
2021-09-09 21:46:51 +0500

    Fix formatting

--- libavfilter/vf_livepeer_dnn.c
+++ libavfilter/vf_livepeer_dnn.c
@@ -47,30 +47,31 @@ typedef struct LivepeerContext {
 #define OFFSET(x) offsetof(LivepeerContext, x)
 #define FLAGS AV_OPT_FLAG_FILTERING_PARAM | AV_OPT_FLAG_VIDEO_PARAM
 static const AVOption livepeer_options[] = {
-        {"dnn_backend", "DNN backend used for model execution", OFFSET(dnnctx.backend_type), AV_OPT_TYPE_INT,
-         {.i64 = 1}, 0, 1, FLAGS, "backend"},
-        {"native", "native backend flag", 0, AV_OPT_TYPE_CONST, {.i64 = 0}, 0, 0, FLAGS, "backend"},
+    {"dnn_backend", "DNN backend used for model execution", OFFSET(dnnctx.backend_type), AV_OPT_TYPE_INT,
+     {.i64 = 1}, 0, 1, FLAGS, "backend"},
+    {"native", "native backend flag", 0, AV_OPT_TYPE_CONST, {.i64 = 0}, 0, 0, FLAGS, "backend"},
 #if (CONFIG_LIBTENSORFLOW == 1)
-        {"tensorflow", "tensorflow backend flag", 0, AV_OPT_TYPE_CONST, {.i64 = 1}, 0, 0, FLAGS, "backend"},
+    {"tensorflow", "tensorflow backend flag", 0, AV_OPT_TYPE_CONST, {.i64 = 1}, 0, 0, FLAGS, "backend"},
 #endif
-        {"model", "path to model file specifying network architecture and its parameters",
-         OFFSET(dnnctx.model_filename), AV_OPT_TYPE_STRING, {.str=NULL}, 0, 0, FLAGS},
-        {"input", "input name of the model", OFFSET(dnnctx.model_inputname), AV_OPT_TYPE_STRING, {.str = "x"}, 0, 0,
-         FLAGS},
-        {"output", "output name of the model", OFFSET(dnnctx.model_outputname), AV_OPT_TYPE_STRING, {.str = "y"}, 0, 0,
-         FLAGS},
-        // default session_config = {allow_growth: true}
-        {"backend_configs", "backend configs", OFFSET(dnnctx.backend_options), AV_OPT_TYPE_STRING,
-         {.str = "sess_config=0x01200232"}, 0, 0, FLAGS},
-        {"logfile", "path to logfile", OFFSET(log_filename), AV_OPT_TYPE_STRING, {.str = NULL}, 0, 0, FLAGS},
-        {NULL}
+    {"model", "path to model file specifying network architecture and its parameters",
+     OFFSET(dnnctx.model_filename), AV_OPT_TYPE_STRING, {.str=NULL}, 0, 0, FLAGS},
+    {"input", "input name of the model", OFFSET(dnnctx.model_inputname), AV_OPT_TYPE_STRING, {.str = "x"}, 0, 0,
+     FLAGS},
+    {"output", "output name of the model", OFFSET(dnnctx.model_outputname), AV_OPT_TYPE_STRING, {.str = "y"}, 0, 0,
+     FLAGS},
+    // default session_config = {allow_growth: true}
+    {"backend_configs", "backend configs", OFFSET(dnnctx.backend_options), AV_OPT_TYPE_STRING,
+     {.str = "sess_config=0x01200232"}, 0, 0, FLAGS},
+    {"logfile", "path to logfile", OFFSET(log_filename), AV_OPT_TYPE_STRING, {.str = NULL}, 0, 0, FLAGS},
+    {NULL}
 };
 
 AVFILTER_DEFINE_CLASS(livepeer);
 
 static int post_proc(AVFrame *out, DNNData *dnn_output, AVFilterContext *context);
 
-static av_cold int init(AVFilterContext *context) {
+static av_cold int init(AVFilterContext *context)
+{
     LivepeerContext *ctx = context->priv;
     DNNData input;
     int ret = 0;
@@ -89,11 +90,10 @@ static av_cold int init(AVFilterContext *context) {
     ctx->input_height = input.height;
     // pre-executes the model and gets output information
     if (DNN_SUCCESS != ctx->dnnctx.model->get_output(ctx->dnnctx.model->model, ctx->dnnctx.model_inputname,
-                                  input.width, input.height,
-                                  ctx->dnnctx.model_outputname,
-                                  &ctx->output_width,
-                                  &ctx->output_height))
-    {
+                                                     input.width, input.height,
+                                                     ctx->dnnctx.model_outputname,
+                                                     &ctx->output_width,
+                                                     &ctx->output_height)) {
         av_log(ctx, AV_LOG_ERROR, "failed to init model\n");
         return AVERROR(EIO);
     }
@@ -103,7 +103,8 @@ static av_cold int init(AVFilterContext *context) {
     return ret;
 }
 
-static int query_formats(AVFilterContext *context) {
+static int query_formats(AVFilterContext *context)
+{
     const enum AVPixelFormat pixel_formats[] = {AV_PIX_FMT_RGB24, AV_PIX_FMT_BGR24, AV_PIX_FMT_NV12,
                                                 AV_PIX_FMT_YUV420P, AV_PIX_FMT_YUV422P, AV_PIX_FMT_YUV444P,
                                                 AV_PIX_FMT_YUV410P, AV_PIX_FMT_YUV411P, AV_PIX_FMT_GRAY8,
@@ -119,7 +120,8 @@ static int query_formats(AVFilterContext *context) {
     return ff_set_common_formats(context, formats_list);
 }
 
-static int prepare_sws_context(AVFilterLink *inlink) {
+static int prepare_sws_context(AVFilterLink *inlink)
+{
     int result = 0;
     enum AVPixelFormat fmt = inlink->format;
     AVFilterContext *context = inlink->dst;
@@ -153,7 +155,8 @@ static int prepare_sws_context(AVFilterLink *inlink) {
     return 0;
 }
 
-static int config_input(AVFilterLink *inlink) {
+static int config_input(AVFilterLink *inlink)
+{
     AVFilterContext *context = inlink->dst;
     LivepeerContext *ctx = context->priv;
     int check;
@@ -165,7 +168,8 @@ static int config_input(AVFilterLink *inlink) {
     return 0;
 }
 
-static int filter_frame(AVFilterLink *inlink, AVFrame *in) {
+static int filter_frame(AVFilterLink *inlink, AVFrame *in)
+{
     AVFilterContext *context = inlink->dst;
     LivepeerContext *ctx = context->priv;
     AVFilterLink *outlink = context->outputs[0];
@@ -202,7 +206,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in) {
     return ff_filter_frame(outlink, in);
 }
 
-static int post_proc(AVFrame *out, DNNData *dnn_output, AVFilterContext *context) {
+static int post_proc(AVFrame *out, DNNData *dnn_output, AVFilterContext *context)
+{
     LivepeerContext *ctx = context->priv;
     float *pfdata = dnn_output->data;
     int lendata = dnn_output->height;
@@ -224,7 +229,8 @@ static int post_proc(AVFrame *out, DNNData *dnn_output, AVFilterContext *context
     return DNN_SUCCESS;
 }
 
-static av_cold void uninit(AVFilterContext *context) {
+static av_cold void uninit(AVFilterContext *context)
+{
     LivepeerContext *ctx = context->priv;
 
     sws_freeContext(ctx->sws_rgb_scale);
@@ -239,31 +245,31 @@ static av_cold void uninit(AVFilterContext *context) {
 }
 
 static const AVFilterPad livepeer_inputs[] = {
-        {
-                .name         = "default",
-                .type         = AVMEDIA_TYPE_VIDEO,
-                .config_props = config_input,
-                .filter_frame = filter_frame,
-        },
-        {NULL}
+    {
+        .name         = "default",
+        .type         = AVMEDIA_TYPE_VIDEO,
+        .config_props = config_input,
+        .filter_frame = filter_frame,
+    },
+    {NULL}
 };
 
 static const AVFilterPad livepeer_outputs[] = {
-        {
-                .name = "default",
-                .type = AVMEDIA_TYPE_VIDEO,
-        },
-        {NULL}
+    {
+        .name = "default",
+        .type = AVMEDIA_TYPE_VIDEO,
+    },
+    {NULL}
 };
 
 AVFilter ff_vf_livepeer_dnn = {
-        .name          = "livepeer_dnn",
-        .description   = NULL_IF_CONFIG_SMALL("Perform DNN-based scene classification on input."),
-        .priv_size     = sizeof(LivepeerContext),
-        .init          = init,
-        .uninit        = uninit,
-        .query_formats = query_formats,
-        .inputs        = livepeer_inputs,
-        .outputs       = livepeer_outputs,
-        .priv_class    = &livepeer_class,
+    .name          = "livepeer_dnn",
+    .description   = NULL_IF_CONFIG_SMALL("Perform DNN-based scene classification on input."),
+    .priv_size     = sizeof(LivepeerContext),
+    .init          = init,
+    .uninit        = uninit,
+    .query_formats = query_formats,
+    .inputs        = livepeer_inputs,
+    .outputs       = livepeer_outputs,
+    .priv_class    = &livepeer_class,
 };
